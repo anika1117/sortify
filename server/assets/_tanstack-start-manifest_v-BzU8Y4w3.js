6669cb190b9110e3bfae8b02b542c6e0b5b40aa0
Anika <bompallyanika@gmail.com>
2026-04-27 13:20:32 +0530

    Updates

--- server/assets/_tanstack-start-manifest_v-BzU8Y4w3.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/Sai Anika/Downloads/sortify/algorithm-weaver-main/algorithm-weaver-main/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/sortify/assets/index-CrFNQ8R-.js"] }, "/": { filePath: "C:/Users/Sai Anika/Downloads/sortify/algorithm-weaver-main/algorithm-weaver-main/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/sortify/assets/index-oXQEoGjI.js"] } }, clientEntry: "/sortify/assets/index-CrFNQ8R-.js" });
-export {
-  tsrStartManifest
-};
